e891cfe7d33401169f84be3dd95fffd910d925d0
aOpenbrier <aopenbrier@gmail.com>
2019-12-25 17:37:44 -0800

    nav opacity on scroll

---
+++ src/js/nav.js
@@ -0,0 +1,25 @@
+// Allow callback to run at most 1 time per 100ms
+//     window.addEventListener("scroll", throttle(navOpacity, 100));
+//
+window.addEventListener("scroll", navOpacity());
+
+function throttle(callback, limit) {
+    var wait = false                   // Initially, we're not waiting
+    return function () {               // We return a throttled function
+        if (!wait) {                   // If we're not waiting
+            callback.call()            // Execute users function
+            wait = true                // Prevent future invocations
+            setTimeout(function () {   // After a period of time
+                wait = false           // And allow future invocations
+            }, limit)
+        }
+    }
+}
+
+function navOpacity() {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("top-bar").classList.add('bg-white')
+        } else {
+            document.getElementById("top-bar").classList.remove('bg-white');
+        }
+}
