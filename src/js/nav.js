828a63a7b4503879c2179d49f446fc5691d200c0
aOpenbrier <aopenbrier@gmail.com>
2019-12-26 01:03:52 -0800

    lower scroll distance

--- src/js/nav.js
+++ src/js/nav.js
@@ -17,9 +17,9 @@ function throttle(callback, limit) {
 }
 
 function navOpacity() {
-        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementById("top-bar").classList.add('nav-bg')
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+            document.getElementById("top-bar").classList.add('top-scrolled')
         } else {
-            document.getElementById("top-bar").classList.remove('nav-bg');
+            document.getElementById("top-bar").classList.remove('top-scrolled');
         }
 }
