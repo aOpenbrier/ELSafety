574f8b6602eaadd23fbbe2e3b6701599c98bb0bf
aOpenbrier <aopenbrier@gmail.com>
2020-01-12 17:41:22 -0800

    expand submenu onclick

--- src/js/nav.js
+++ src/js/nav.js
@@ -26,9 +26,39 @@ function openMenu() {
     document.getElementById('nav-collapse').classList.toggle('mobile-nav-open')
 }
 
+for (let i = 0; i < document.getElementsByClassName('sub-menu').length; i++) {
+    document.getElementsByClassName('sub-menu')[i].onclick = openSubMenu
+}
+
+function openSubMenu(event) {
+    let target;
+    if (event.target.tagName === 'LI') {
+        target = event.target
+    }
+    else {
+        // if clicked on child
+        target = event.target.parentNode
+    }
+    // close other submenus before expanded target
+    for (let i = 0; i < document.getElementsByClassName('sub-menu').length; i++) {
+        if (document.getElementsByClassName('sub-menu')[i] === target){
+            target.classList.toggle('sub-expanded')
+        }
+        else {
+            document.getElementsByClassName('sub-menu')[i].classList.remove('sub-expanded')
+        }
+    }
+
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('#nav-trigger')){
         document.getElementById('nav-collapse').classList.remove('mobile-nav-open')
+    }
+    if(!event.target.classList.contains('sub-menu') && !event.target.parentNode.classList.contains('sub-menu')){
+        for (let i = 0; i< document.getElementsByClassName('sub-menu').length; i++){
+            document.getElementsByClassName('sub-menu')[i].classList.remove('sub-expanded')
+        }
     }
 }
