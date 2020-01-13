d8cd023adb18b38c88e8285544da2a0ee3713fd8
aOpenbrier <aopenbrier@gmail.com>
2020-01-12 16:17:05 -0800

    Updates

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,25 @@
 "use strict";
 
-if (matchMedia('(pointer:coarse)')) {
-  document.body.style.height = window.innerHeight;
+function fixBodyHeight() {
+  if (window.matchMedia('(pointer:coarse)').matches) {
+    console.log('matches coarse');
+    document.body.style.minHeight = window.innerHeight + 'px';
+  }
 }
+
+window.addEventListener("resize", throttle(fixBodyHeight, 33));
+
+function throttle(callback, limit) {
+  var wait = false;
+  return function () {
+    if (!wait) {
+      callback.call();
+      wait = true;
+      setTimeout(function () {
+        wait = false;
+      }, limit);
+    }
+  };
+}
+
+fixBodyHeight();
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -27,11 +27,41 @@ function navOpacity() {
 
 function openMenu() {
   document.getElementById('nav-collapse').classList.toggle('mobile-nav-open');
+}
+
+for (var i = 0; i < document.getElementsByClassName('sub-menu').length; i++) {
+  document.getElementsByClassName('sub-menu')[i].onclick = openSubMenu;
+}
+
+function openSubMenu(event) {
+  var target;
+
+  if (event.target.tagName === 'LI') {
+    target = event.target;
+  } else {
+    // if clicked on child
+    target = event.target.parentNode;
+  } // close other submenus before expanded target
+
+
+  for (var _i = 0; _i < document.getElementsByClassName('sub-menu').length; _i++) {
+    if (document.getElementsByClassName('sub-menu')[_i] === target) {
+      target.classList.toggle('sub-expanded');
+    } else {
+      document.getElementsByClassName('sub-menu')[_i].classList.remove('sub-expanded');
+    }
+  }
 } // Close the dropdown if the user clicks outside of it
 
 
 window.onclick = function (event) {
   if (!event.target.matches('#nav-trigger')) {
     document.getElementById('nav-collapse').classList.remove('mobile-nav-open');
+  }
+
+  if (!event.target.classList.contains('sub-menu') && !event.target.parentNode.classList.contains('sub-menu')) {
+    for (var _i2 = 0; _i2 < document.getElementsByClassName('sub-menu').length; _i2++) {
+      document.getElementsByClassName('sub-menu')[_i2].classList.remove('sub-expanded');
+    }
   }
 };
