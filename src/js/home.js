5f6550c3a01cb08a382a52316723997303858cdc
aOpenbrier <aopenbrier@gmail.com>
2020-01-24 02:23:01 -0800

    called by fixheight in nav.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,10 +1,9 @@
+isHomePage = true
+
 function fixBodyHeight() {
     if (window.matchMedia('(pointer:coarse)').matches) {
-        console.log('matches coarse')
         document.body.style.minHeight = window.innerHeight + 'px'
     }
 }
-
-window.addEventListener("resize", throttle(fixBodyHeight, 33))
 
 fixBodyHeight()
